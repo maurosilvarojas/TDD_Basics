5ff569bb47ae4259e51a83b9694701777a2149fe
maurosilvarojas <reggaecol@gmail.com>
2016-10-13 22:13:39 +0100

    First Commit

---
+++ src/helloTest.java
@@ -0,0 +1,27 @@
+
+public class helloTest {
+    public String name;
+    public Integer number,xd;
+    private Integer x=5;
+
+
+    public static void main(String[] args) {
+
+    }
+
+    public helloTest(String name) {
+        this.name = name;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+    public int sqFunction(int z) {
+        x=z*z;
+        return x;
+    }
+
+}
